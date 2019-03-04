e944a404e6fc3f30f60f9c4640b65122486e44d3
Svetoslav <svetoslav_atanasov@mail.bg>
2019-03-04 11:31:35 +0200

    made additional minor fixes

--- src/SideMenu/SideMenu.jsx
+++ src/SideMenu/SideMenu.jsx
@@ -1,9 +1,9 @@
 import React from "react";
-import { NavLink } from 'react-router-dom';
+import { NavLink } from "react-router-dom";
 import styles from "./SideMenu.module.css";
-import theatre from '../assets/images/theatre.png';
-import pochivki from '../assets/images/pochivki.png';
-import meal from '../assets/images/meal.png';
+import theatre from "../assets/images/theatre.png";
+import pochivki from "../assets/images/pochivki.png";
+import meal from "../assets/images/meal.png";
 
 const sideMenu = props => {
   let classes = [styles.sideMenu];
@@ -14,13 +14,22 @@ const sideMenu = props => {
     <nav className={classes.join(" ")}>
       <ul>
         <li>
-          <NavLink to="/culture"> Култура </NavLink><span><img  width="30" height="30" src={theatre} /></span>
+          <NavLink to="/culture"> Culture </NavLink>
+          <span>
+            <img width="30" height="30" src={theatre} />
+          </span>
         </li>
         <li>
-          <NavLink to="/vacations"> Почивки </NavLink><span><img  width="30" height="30" src={pochivki} /></span>
+          <NavLink to="/eating"> Eating Out </NavLink>
+          <span>
+            <img width="30" height="30" src={meal} />
+          </span>
         </li>
         <li>
-          <NavLink to="/eating"> Хапване </NavLink><span><img  width="30" height="30" src={meal} /></span>
+          <NavLink to="/vacations"> Vacations </NavLink>
+          <span>
+            <img width="30" height="30" src={pochivki} />
+          </span>
         </li>
       </ul>
     </nav>
