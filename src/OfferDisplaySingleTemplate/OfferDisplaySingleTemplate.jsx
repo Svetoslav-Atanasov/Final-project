e85c14d04be31c3b7c86530ad0492dc967c09474
Svetoslav <svetoslav_atanasov@mail.bg>
2019-03-09 23:44:47 +0200

    offer can't be added to cart when it has expired

--- src/OfferDisplaySingleTemplate/OfferDisplaySingleTemplate.jsx
+++ src/OfferDisplaySingleTemplate/OfferDisplaySingleTemplate.jsx
@@ -9,39 +9,42 @@ import { Link } from "react-router-dom";
 import Select from "../UI/Select/Select";
 import Image from "../Image/Image";
 import Countdown, { zeroPad } from "react-countdown-now";
-import Zoom from "react-reveal/Zoom";
+import Fade from "react-reveal/Fade";
 
 class Offer extends Component {
   //ot stateless go pravq na statefull component,
   //za da moje da si pazi informaciqta ot input-a  kolko vauchera da bydat kupeni
   // i syotvetno s funkciqta toGetToCart() - da vleznat v kolichkata na tekushtiq user
   state = {
-    broi: "1"
+    broi: "1",
+    timerExpired: this.props.isExpired
   };
-    onClickGetVoucher = () => {
-      const user = this.props.current
-      user ? this.toGetToCart(user.id, this.props.id, this.props.name) : this.props.history.push("/loginPage")
-      
-    }
-    //tazi funkciq - vzima id na potrebitelq  i slaga v koshnicata mu - vaucher 
-    toGetToCart = (idUser, offerId, offerName) => {
-        // number shte e unikalen nomer na vauchera
-        const number = Math.ceil(Math.random()*10000);
-        const broi  =this.state.broi;
-        const orderdVoucher = {number, idUser, broi, offerId, offerName}
-        // pravq nov obekt PorychanVaucher i go slagam v kolickata na tekushtiq potrebitel
-        // const voucher = {idUser, broi, number, offerId, isUsed:false, name};  
-      console.log('GET VUCHER TO CAR BY CLICK ON BUTTON')
-      console.log(orderdVoucher)
-        this.props.getToCart(idUser,orderdVoucher);
-        this.props.goToOrdered(orderdVoucher);
-        // this.props.addVoucher(voucher)
-      } 
+
+  onClickGetVoucher = () => {
+    const user = this.props.current;
+    user
+      ? this.toGetToCart(user.id, this.props.id, this.props.name)
+      : this.props.history.push("/loginPage");
+  };
+  //tazi funkciq - vzima id na potrebitelq  i slaga v koshnicata mu - vaucher
+  toGetToCart = (idUser, offerId, offerName) => {
+    // number shte e unikalen nomer na vauchera
+    const number = Math.ceil(Math.random() * 10000);
+    const broi = this.state.broi;
+    const orderdVoucher = { number, idUser, broi, offerId, offerName };
+    // pravq nov obekt PorychanVaucher i go slagam v kolickata na tekushtiq potrebitel
+    // const voucher = {idUser, broi, number, offerId, isUsed:false, name};
+    console.log("GET VUCHER TO CAR BY CLICK ON BUTTON");
+    console.log(orderdVoucher);
+    this.props.getToCart(idUser, orderdVoucher);
+    this.props.goToOrdered(orderdVoucher);
+    // this.props.addVoucher(voucher)
+  };
 
   //tazi funkciq change() - shte vzima value ot inputa i shte go slaga v state, za da se zapzi
   // i da moje da byde izprateno
   change = e => {
-    console.log(e.target.value)
+    console.log(e.target.value);
     this.setState({ broi: e.target.value });
   };
 
@@ -65,31 +68,22 @@ class Offer extends Component {
     }
   };
 
+  onComplete = () => {
+    //this.state.timerExpired = true;
+    this.setState({ timerExpired: true });
+  };
+
   render() {
-  
-    const dayRandom = Math.floor(Math.random() * 29 + 1);
-    // var expDate = new Date(2019, 2, dayRandom);
-    // // console.log(expDate);
-    // console.log(this.props.expDate);
-    // console.log(this.props);
-
     // takes date from userReducer's offerList
-
-    var formattedDate = this.props.expirationDate.split('.');
-    var expDate = new Date (formattedDate[0], formattedDate[1]-1, formattedDate[2]);
-    // console.log(expDate);
-    var formattedDate = this.props.expDate.split(".");
+    var formattedDate = this.props.expirationDate.split(".");
     var expDate = new Date(
       formattedDate[0],
       formattedDate[1] - 1,
       formattedDate[2]
     );
-    // console.log(expDate);
 
-    // console.log(expDate - Date.now());
-    // console.log(Date.now() - expDate);
     return (
-      <Zoom>
+      <Fade cascade>
         <div className={styles.singleDiv}>
           <h1 className={styles.titleMainScreen}>{this.props.name}</h1>
           <div>
@@ -98,7 +92,10 @@ class Offer extends Component {
             </Link>
           </div>
           <div>{this.props.description}</div>
-          <div>{this.props.price}<span> BGN</span></div>
+          <div>
+            {this.props.price}
+            <span> BGN</span>
+          </div>
           <div>{this.props.category}</div>
           {/* months are counted from 0-11; +1 to start from the month of March */}
           <div>
@@ -113,19 +110,22 @@ class Offer extends Component {
             <Button
               //tuk proverqvam dali ima lognat user -> ako nqma - da go preprashta na login stranicata
               // ako ima da gi sloji v kolichkata
-              // timerOut={() => (Date.now() - expDate > 0) ?
-              // moreStyles={(Date.now() - expDate  0) ? {} : { display: 'none' }} />
-              // // }
-                onClick={this.onClickGetVoucher}
-                title="GET VOUCHER"
+              timerDisabled={this.state.timerExpired}
+              onClick={this.onClickGetVoucher}
+              title="GET VOUCHER"
             />
           </div>
 
           <div className={styles.timer}>
-            Time left: <Countdown date={expDate} renderer={this.renderer} />
+            Time left:{" "}
+            <Countdown
+              date={expDate}
+              renderer={this.renderer}
+              onComplete={this.onComplete}
+            />
           </div>
         </div>
-      </Zoom>
+      </Fade>
     );
   }
 }
